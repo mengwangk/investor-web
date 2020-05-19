d8c1de0536a1639d89cf643b39ac219f446d6c08
Koh Meng Wang <mengwangk@gmail.com>
2020-05-19 23:13:40 +0800

    Initial commit

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -4,6 +4,7 @@ import Home from "../containers/Home";
 import NotFound from "../containers/NotFound";
 import Login from "../containers/Login";
 import Signup from "../containers/Signup";
+import Settings from "../containers/Settings";
 
 export default function Routes() {
   return (
@@ -16,6 +17,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/signup">
         <Signup />
+      </Route>
+      <Route exact path="/settings">
+        <Settings />
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
