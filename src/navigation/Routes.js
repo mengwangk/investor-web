9bdda78edc1fe2a499ded7b25a4ba69fa3c16cd1
Koh Meng Wang <mengwangk@gmail.com>
2020-05-20 22:19:22 +0800

    Initial commit

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,31 +1,39 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "../containers/Home";
-import NotFound from "../containers/NotFound";
-import Login from "../containers/Login";
-import Signup from "../containers/Signup";
-import Settings from "../containers/Settings";
+import asyncComponent from "../components/AsyncComponent";
 import AuthenticatedRoute from "../components/AuthenticatedRoute";
 import UnauthenticatedRoute from "../components/UnauthenticatedRoute";
+
+// import Home from "../containers/Home";
+// import NotFound from "../containers/NotFound";
+// import Login from "../containers/Login";
+// import Signup from "../containers/Signup";
+// import Settings from "../containers/Settings";
+
+const AsyncHome = asyncComponent(() => import("../containers/Home"));
+const AsyncNotFound = asyncComponent(() => import("../containers/NotFound"));
+const AsyncLogin = asyncComponent(() => import("../containers/Login"));
+const AsyncSignup = asyncComponent(() => import("../containers/Signup"));
+const AsyncSettings = asyncComponent(() => import("../containers/Settings"));
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
-        <Home />
+        <AsyncHome />
       </Route>
       <UnauthenticatedRoute exact path="/login">
-        <Login />
+        <AsyncLogin />
       </UnauthenticatedRoute>
       <UnauthenticatedRoute exact path="/signup">
-        <Signup />
+        <AsyncSignup />
       </UnauthenticatedRoute>
       <AuthenticatedRoute exact path="/settings">
-        <Settings />
+        <AsyncSettings />
       </AuthenticatedRoute>
       {/* Finally, catch all unmatched routes */}
       <Route>
-        <NotFound />
+        <AsyncNotFound />
       </Route>
     </Switch>
   );
