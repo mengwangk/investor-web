cb4d9d423131155ea628505743cb0547a42167ae
Koh Meng Wang <mengwangk@gmail.com>
2020-05-21 08:19:25 +0800

    Initial commit

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,3 +1,25 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({ dsn: "https://your-dsn-id-here@sentry.io/123456" });
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
 
 export function onError(error) {
   let errorInfo = {};
