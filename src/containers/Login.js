aa30d2e8beb6dc52aede1a8f016b7e3c996542b2
Koh Meng Wang <mengwangk@gmail.com>
2020-05-24 10:09:28 +0800

    Initial commit

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { useTranslation } from "react-i18next";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { Auth } from "aws-amplify";
-import { useAppContext } from "../libs/contextLib";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Link as RouterLink } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { useFormFields } from "../libs/hooksLib";
 
@@ -35,6 +35,9 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+  },
+  login: {
+    margin: theme.spacing(1, 0,1),
   },
 }));
 
@@ -77,6 +80,26 @@ export default function Login() {
           {t("account.login")}
         </Typography>
         <form className={classes.form} onSubmit={handleSubmit}>
+          <LoaderButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.login}
+          >
+            {t("account.loginWithFacebook")}
+          </LoaderButton>
+
+          <LoaderButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.login}
+          >
+            {t("account.loginWithGoogle")}
+          </LoaderButton>
+
           <TextField
             variant="outlined"
             margin="normal"
