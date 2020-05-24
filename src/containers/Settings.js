38f451cbde71ab59d7321326e5644dc9daa6885b
Koh Meng Wang <mengwangk@gmail.com>
2020-05-24 08:58:56 +0800

    Initial commit

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,21 +1,9 @@
-import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import { useAppContext } from "../libs/contextLib";
-import { Link as RouterLink } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
-import { onError } from "../libs/errorLib";
-import { useFormFields } from "../libs/hooksLib";
 
 const useStyles = makeStyles((theme) => ({
   root: {
