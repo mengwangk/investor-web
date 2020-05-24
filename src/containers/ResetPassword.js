1c122ca1e6a4fa07081e9f31122d3a4166c28f0f
Koh Meng Wang <mengwangk@gmail.com>
2020-05-24 23:04:24 +0800

    Initial commit

--- src/containers/ResetPassword.js
+++ src/containers/ResetPassword.js
@@ -1,11 +1,33 @@
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
 import { Auth } from "aws-amplify";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errorLib";
 import { useFormFields } from "../libs/hooksLib";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export default function ResetPassword() {
+  const classes = useStyles();
   const [fields, handleFieldChange] = useFormFields({
     code: "",
     email: "",
@@ -63,95 +85,106 @@ export default function ResetPassword() {
 
   function renderRequestCodeForm() {
     return (
-      <form onSubmit={handleSendCodeClick}>
-        <FormGroup bsSize="large" controlId="email">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={fields.email}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          isLoading={isSendingCode}
-          disabled={!validateCodeForm()}
-        >
-          Send Confirmation
-        </LoaderButton>
-      </form>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}></div>
+      </Container>
     );
   }
 
-  function renderConfirmationForm() {
-    return (
-      <form onSubmit={handleConfirmClick}>
-        <FormGroup bsSize="large" controlId="code">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            value={fields.code}
-            onChange={handleFieldChange}
-          />
-          <HelpBlock>
-            Please check your email ({fields.email}) for the confirmation code.
-          </HelpBlock>
-        </FormGroup>
-        <hr />
-        <FormGroup bsSize="large" controlId="password">
-          <ControlLabel>New Password</ControlLabel>
-          <FormControl
-            type="password"
-            value={fields.password}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup bsSize="large" controlId="confirmPassword">
-          <ControlLabel>Confirm Password</ControlLabel>
-          <FormControl
-            type="password"
-            value={fields.confirmPassword}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          isLoading={isConfirming}
-          disabled={!validateResetForm()}
-        >
-          Confirm
-        </LoaderButton>
-      </form>
-    );
-  }
+  // function renderRequestCodeForm() {
+  //   return (
+  //     <form onSubmit={handleSendCodeClick}>
+  //       <FormGroup bsSize="large" controlId="email">
+  //         <ControlLabel>Email</ControlLabel>
+  //         <FormControl
+  //           autoFocus
+  //           type="email"
+  //           value={fields.email}
+  //           onChange={handleFieldChange}
+  //         />
+  //       </FormGroup>
+  //       <LoaderButton
+  //         block
+  //         type="submit"
+  //         bsSize="large"
+  //         isLoading={isSendingCode}
+  //         disabled={!validateCodeForm()}
+  //       >
+  //         Send Confirmation
+  //       </LoaderButton>
+  //     </form>
+  //   );
+  // }
 
-  function renderSuccessMessage() {
-    return (
-      <div className="success">
-        <Glyphicon glyph="ok" />
-        <p>Your password has been reset.</p>
-        <p>
-          <Link to="/login">
-            Click here to login with your new credentials.
-          </Link>
-        </p>
-      </div>
-    );
-  }
+  // function renderConfirmationForm() {
+  //   return (
+  //     <form onSubmit={handleConfirmClick}>
+  //       <FormGroup bsSize="large" controlId="code">
+  //         <ControlLabel>Confirmation Code</ControlLabel>
+  //         <FormControl
+  //           autoFocus
+  //           type="tel"
+  //           value={fields.code}
+  //           onChange={handleFieldChange}
+  //         />
+  //         <HelpBlock>
+  //           Please check your email ({fields.email}) for the confirmation code.
+  //         </HelpBlock>
+  //       </FormGroup>
+  //       <hr />
+  //       <FormGroup bsSize="large" controlId="password">
+  //         <ControlLabel>New Password</ControlLabel>
+  //         <FormControl
+  //           type="password"
+  //           value={fields.password}
+  //           onChange={handleFieldChange}
+  //         />
+  //       </FormGroup>
+  //       <FormGroup bsSize="large" controlId="confirmPassword">
+  //         <ControlLabel>Confirm Password</ControlLabel>
+  //         <FormControl
+  //           type="password"
+  //           value={fields.confirmPassword}
+  //           onChange={handleFieldChange}
+  //         />
+  //       </FormGroup>
+  //       <LoaderButton
+  //         block
+  //         type="submit"
+  //         bsSize="large"
+  //         isLoading={isConfirming}
+  //         disabled={!validateResetForm()}
+  //       >
+  //         Confirm
+  //       </LoaderButton>
+  //     </form>
+  //   );
+  // }
 
-  return (
-    <div className="ResetPassword">
-      {!codeSent
-        ? renderRequestCodeForm()
-        : !confirmed
-        ? renderConfirmationForm()
-        : renderSuccessMessage()}
-    </div>
-  );
+  // function renderSuccessMessage() {
+  //   return (
+  //     <div className="success">
+  //       <Glyphicon glyph="ok" />
+  //       <p>Your password has been reset.</p>
+  //       <p>
+  //         <Link to="/login">
+  //           Click here to login with your new credentials.
+  //         </Link>
+  //       </p>
+  //     </div>
+  //   );
+  // }
+
+  //   return (
+  //      <div className="ResetPassword">
+  //       {!codeSent
+  //         ? renderRequestCodeForm()
+  //         : !confirmed
+  //         ? renderConfirmationForm()
+  //         : renderSuccessMessage()}
+  //     </div>
+  //   );
+
+  return <div className="ResetPassword">{renderRequestCodeForm()}</div>;
 }
