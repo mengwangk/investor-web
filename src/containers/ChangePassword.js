037baa653773329d74d3018e6ff9023e75813d62
Koh Meng Wang <mengwangk@gmail.com>
2020-05-30 10:48:55 +0800

    Initial commit

--- src/containers/ChangePassword.js
+++ src/containers/ChangePassword.js
@@ -1,6 +1,13 @@
+import Avatar from "@material-ui/core/Avatar";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { Auth } from "aws-amplify";
-import { React, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import { onError } from "../libs/errorLib";
@@ -66,77 +73,73 @@ export default function ChangePassword() {
   }
 
   return (
-    <div>ok</div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {t("account.changePassword")}
+        </Typography>
+        <form className={classes.form}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label={t("resetPassword.password")}
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 }
 
-  // return (
-  //   <Container component="main" maxWidth="xs">
-  //     <CssBaseline />
-  //     <div className={classes.paper}>
-  //       <Avatar className={classes.avatar}>
-  //         <LockOutlinedIcon />
-  //       </Avatar>
-  //       <Typography component="h1" variant="h5">
-  //         {t("account.changePassword")}
-  //       </Typography>
-  //       <form className={classes.form}>
-  //         <Grid container spacing={2}>
-  //           <Grid item xs={12}>
-  //             <TextField
-  //               variant="outlined"
-  //               required
-  //               fullWidth
-  //               label={t("resetPassword.password")}
-  //               name="password"
-  //               type="password"
-  //               id="password"
-  //               autoComplete="current-password"
-  //             />
-  //           </Grid>
-  //         </Grid>
-  //       </form>
-  //     </div>
-  //   </Container>
-  // );
-
-  //   return (
-  //     <div className="ChangePassword">
-  //       <form onSubmit={handleChangeClick}>
-  //         <FormGroup bsSize="large" controlId="oldPassword">
-  //           <ControlLabel>Old Password</ControlLabel>
-  //           <FormControl
-  //             type="password"
-  //             onChange={handleFieldChange}
-  //             value={fields.oldPassword}
-  //           />
-  //         </FormGroup>
-  //         <hr />
-  //         <FormGroup bsSize="large" controlId="password">
-  //           <ControlLabel>New Password</ControlLabel>
-  //           <FormControl
-  //             type="password"
-  //             onChange={handleFieldChange}
-  //             value={fields.password}
-  //           />
-  //         </FormGroup>
-  //         <FormGroup bsSize="large" controlId="confirmPassword">
-  //           <ControlLabel>Confirm Password</ControlLabel>
-  //           <FormControl
-  //             type="password"
-  //             onChange={handleFieldChange}
-  //             value={fields.confirmPassword}
-  //           />
-  //         </FormGroup>
-  //         <LoaderButton
-  //           block
-  //           type="submit"
-  //           bsSize="large"
-  //           disabled={!validateForm()}
-  //           isLoading={isChanging}
-  //         >
-  //           Change Password
-  //         </LoaderButton>
-  //       </form>
-  //     </div>
-  //   );
+//   return (
+//     <div className="ChangePassword">
+//       <form onSubmit={handleChangeClick}>
+//         <FormGroup bsSize="large" controlId="oldPassword">
+//           <ControlLabel>Old Password</ControlLabel>
+//           <FormControl
+//             type="password"
+//             onChange={handleFieldChange}
+//             value={fields.oldPassword}
+//           />
+//         </FormGroup>
+//         <hr />
+//         <FormGroup bsSize="large" controlId="password">
+//           <ControlLabel>New Password</ControlLabel>
+//           <FormControl
+//             type="password"
+//             onChange={handleFieldChange}
+//             value={fields.password}
+//           />
+//         </FormGroup>
+//         <FormGroup bsSize="large" controlId="confirmPassword">
+//           <ControlLabel>Confirm Password</ControlLabel>
+//           <FormControl
+//             type="password"
+//             onChange={handleFieldChange}
+//             value={fields.confirmPassword}
+//           />
+//         </FormGroup>
+//         <LoaderButton
+//           block
+//           type="submit"
+//           bsSize="large"
+//           disabled={!validateForm()}
+//           isLoading={isChanging}
+//         >
+//           Change Password
+//         </LoaderButton>
+//       </form>
+//     </div>
+//   );
